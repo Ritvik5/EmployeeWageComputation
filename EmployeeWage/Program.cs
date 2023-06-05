a07309b22b06158ffc8446632052a997b3104142
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-05 20:02:03 +0530

    [Ritvik]Add: Welcome Message

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
