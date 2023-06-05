46719c24b42d49b504896cbeaeeb7fb38db267ea
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-05 22:01:14 +0530

    [Ritvik]Add: Calculating Daily Employee Wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,18 +5,24 @@
         static void Main(string[] args)
         {
             int isPresent = 1;
+            int empWagePerHour = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
 
             int empCheck = random.Next(0,2);
 
             if (empCheck == isPresent )
             {
-                Console.WriteLine("Employee is Present.");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * empWagePerHour;
+            Console.WriteLine("Daily Employee Wage is " +empWage);
         }
     }
 }
