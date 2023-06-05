5f192b8c76196cff1fc551e4c18519c5ab372d44
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-05 20:13:18 +0530

    [Ritvik]Add: Checking Employee is Present or Absent

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            int isPresent = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0,2);
+
+            if (empCheck == isPresent )
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
